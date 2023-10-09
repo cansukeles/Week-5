cc69270624d3620b314b7d433b6a0953989a9cdc
cansukeles <zeynepcansuoral@gmail.com>
2023-10-09 22:12:35 +0300

    Bugs fixed

--- Try-CatchBlogu/src/Main.java
+++ Try-CatchBlogu/src/Main.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int checkArray(int [] array, int index) throws Exception{
+        if(index <0 || index > array.length){
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            System.out.println("A Valid Action.");
+        }
+        return array [index];
+    }
+
     public static void main(String[] args) {
         int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         Scanner scan = new Scanner(System.in);
-
-
-        System.out.print("Enter an index: ");
+        System.out.println("Enter an index number: ");
         int index = scan.nextInt();
 
         try{
-            int element = arr[index];
-            System.out.println(element);
-        } catch(ArrayIndexOutOfBoundsException e){
+            checkArray(arr, index);
+            System.out.println("The element of your index: " + arr[index]);
+        } catch (Exception e) {
+            System.out.println("An Invalid Action! ");
             System.out.println(e.toString());
-        } finally {
-            System.out.println("This is the final point of code");
         }
-
-
     }
 }
